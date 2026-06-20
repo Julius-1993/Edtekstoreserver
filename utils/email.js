dbd63e346a10c6183ed03796193a12d0f6912353
Julius Aako <aakojuiusoluwanifemi@gmail.com>
2026-06-20 12:54:01 +0100

    email checking

--- utils/email.js
+++ utils/email.js
@@ -19,6 +19,7 @@ const nodemailer = require('nodemailer');
 const createTransporter = () => {
   return nodemailer.createTransport({
     service: 'gmail',
+    family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
